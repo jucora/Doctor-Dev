0c0fa5f0d93ea2417924d714464a767166d9d9df
jucora <artjulius@hotmail.com>
2020-02-26 09:09:14 -0500

    git status

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,8 @@
 class TransactionsController < ApplicationController
+	def index
+		@transactions = Transaction.where_not(group_id: nil)
+		@external_transactions = Transaction.where(group_id: nil)
+	end
+
+
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,4 @@
 class Transaction < ApplicationRecord
 	belongs_to :user, foreign_key: 'author_id'
+	belongs_to :group
 end
