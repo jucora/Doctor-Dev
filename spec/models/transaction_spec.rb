1aba8632b6adf36d1949738fd391ed5cdf028b01
jucora <artjulius@hotmail.com>
2020-02-26 15:34:16 -0500

    transactions_spec and groups_spec tests in capybara, user_spec associations test, transaction_spec associations test, group_spec associations test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'belongs to a user' do
+    transaction = Transaction.reflect_on_association(:user)
+    expect(transaction.macro).to eq(:belongs_to)
+  end
+  it 'belongs to a group' do
+    transaction = Transaction.reflect_on_association(:group)
+    expect(transaction.macro).to eq(:belongs_to)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user1) }
 
+  it 'should have many transactions' do
+    user = User.reflect_on_association(:transactions)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'should have many groups' do
+    user = User.reflect_on_association(:groups)
+    expect(user.macro).to eq(:has_many)
+  end
+
   describe '#name' do
     it "can't be blank" do
       user.name = ''
