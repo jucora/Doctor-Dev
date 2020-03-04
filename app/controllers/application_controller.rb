dd4693fa2db886b2f15a51fa778312ed2276b684
jucora <artjulius@hotmail.com>
2020-03-04 14:26:56 -0500

    fixing current_user method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by_remember_token!(cookies[:remember_token]) if cookies[:remember_token]
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token]) if cookies[:remember_token]
   end
 
   def sign_out
