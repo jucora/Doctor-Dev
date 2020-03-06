64b88ff988106fa183645ced3297d4fa3dcabe75
jucora <artjulius@hotmail.com>
2020-03-06 09:36:52 -0500

    Routes resources only and except

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 	root 'sessions#new'
 	delete 'logout', to: 'sessions#delete'
 
-	resources :users
-	resources :sessions
-	resources :groups
-	resources :transactions
+	resources :users, only:[:new, :create, :show]
+	resources :sessions, only:[:new, :create]
+	resources :groups, except: [:edit, :update, :destroy] 
+	resources :transactions, except:[:edit, :update, :destroy] 
 end
